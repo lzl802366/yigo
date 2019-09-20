a558f03ff28335612c13812836d3c93f19faa6c5
lzl <740953806@qq.com>
2019-09-20 21:23:35 +0800

    梁振龙第一次上传

---
+++ src/main/java/com/lzl/yigo/YigoApplication.java
@@ -0,0 +1,13 @@
+package com.lzl.yigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YigoApplication.class, args);
+    }
+
+}
